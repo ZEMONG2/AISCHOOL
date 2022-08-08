436bae898e8f01b995f35ccc47902b9d56141e31
ZEMONG2 <zerosole0@gmail.com>
2022-08-08 21:28:45 +0900

    0808 마무리

--- react/nodejs/learn-express/app.js
+++ react/nodejs/learn-express/app.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
 const path = require("path");
 
+dotenv.config();
 const app = express();
-
 app.set("port", process.env.PORT || 3000);
+
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
 
 app.use((req, res, next) => {
   console.log("모든 요청에 다 실행됩니다.");
@@ -18,7 +40,6 @@ app.get(
   },
   (req, res) => {
     throw new Error("에러는 에러 처리 미들웨어로 갑니다.");
-    // res.sendFile(path.join(__dirname, "/index.html"));
   }
 );
 
